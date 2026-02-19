9d89c859ac2b682e2c68226307e2918512707d55
horlarmhi09-create <horlarmhi09@gmail.com>
2026-02-19 20:31:47 +0100

    turned DEBUG=False

--- plaza_hotel/settings.py
+++ plaza_hotel/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i07peomw-!p$q3-p$j*v$vo0l1n%2o+g$+qcjqlepje&wv)hi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
